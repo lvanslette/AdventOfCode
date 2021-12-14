7c05bf2f72da7132e6d14ea4f2e16d6a90c2be89
vansll <lvanslette>
2021-12-13 21:30:46 -0500

    poop2

--- Problem1/num_increased.py
+++ Problem1/num_increased.py
@@ -1,5 +1,13 @@
 with open('input.txt') as f:
     measurements = f.readlines()
+    prev_distance = -1
+    increased = 0
     for distance in measurements:
-        print(distance)
+        
+        if int(distance) > prev_distance and prev_distance != -1:
+            increased +=1
+        prev_distance = int(distance)
+        
+
+    print(increased)
         
